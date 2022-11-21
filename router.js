507e0cfc21f76f63c4f575762640687e45df756c
Cora Zhang <xiaoqing_zhang525@126.com>
2022-11-21 20:45:51 +0800

    router build: js router

---
+++ router.js
@@ -0,0 +1,38 @@
+class Routers {
+  constructor() {
+    this.routes = {};
+    this.currentUrl = '';
+    this.refresh = this.refresh.bind(this);
+    window.addEventListener('load', this.refresh, false);
+    window.addEventListener('hashchange', this.refresh, false);
+  }
+
+  route(path, callback) {
+    this.routes[path] = callback || function () {};
+  }
+
+  refresh() {
+    this.currentUrl = location.hash.slice(1) || '/';
+    console.log(this.currentUrl);
+    this.routes[this.currentUrl]();
+    console.log(this.routes);
+  }
+}
+
+window.Router = new Routers();
+
+var content = document.querySelector('body');
+// change Page anything
+function changeBgColor(color) {
+  content.style.backgroundColor = color;
+}
+Router.route('/', function () {
+  changeBgColor('yellow');
+});
+
+Router.route('/blue', function () {
+  changeBgColor('blue');
+});
+Router.route('/green', function () {
+  changeBgColor('green');
+});
